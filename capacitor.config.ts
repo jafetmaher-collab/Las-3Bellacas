2b623c9e3a3635fbc33a014f8109eb660811ce65
ELJafi28 <jafetmaher@gmail.com>
2026-09-02 08:55:55 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Las-3Bellacas',
+  webDir: 'www'
+};
+
+export default config;
